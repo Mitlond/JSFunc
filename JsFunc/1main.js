0f710a14832ef9e6a51e154a42495e0584db983a
Mitlond <comp122012016@mail.ru>
2016-10-04 10:31:34 +0300

    Function031016

---
+++ JsFunc/1main.js
@@ -0,0 +1,26 @@
+﻿//возвращение функции
+function myTank(res1, res2, callback) {
+    alert('Начинаем проверку.\n\nПараметры: ' + res1 + ', ' + res2);
+  
+    if (callback && typeof(callback) === "function") {
+        callback();
+    }
+}
+myTank('проверка', 'продолжаеться', function() { 
+     alert ('Проверка закончена.');
+});
+
+var Nat=function(){
+	alert("Privet");
+	Nat=function(){
+		alert("Poka");
+		Nat();// privet
+		Nat();// poka
+	}
+}
+
+
+//немедленно вызываемые функции
+(function sum(a, b) {
+    alert(a + b); // 5
+})(2, 3);
